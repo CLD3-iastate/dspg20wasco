53b101e148c0987f571e6137e5c3513442705a63
marysolomon <50915820+marysolomon@users.noreply.github.com>
2020-07-16 11:16:09 -0400

    Update loadoverlays.R

--- src/dashboard/loadoverlays.R
+++ src/dashboard/loadoverlays.R
@@ -3,5 +3,5 @@ library(tidyverse)
 
 ### loading baselayer data, check file names
 ## food
-allfood <- read_csv("~/git/DSPG2020/wasco/data/shps/allfood.csv")
+allfood <- read_csv("~/git/dspg20wasco/data/shps/allfood.csv")
 allfood <- allfood %>% st_as_sf(coords = c("X", "Y"), crs = 4326, agr  = "constant")
